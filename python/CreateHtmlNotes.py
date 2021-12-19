99e3be224d5b97f438cef216502c531309a1d1a5
Sebastian <sebastian@Silver>
2021-12-19 19:46:32 +0100

    Contained in hyperlink for concept particles

--- python/CreateHtmlNotes.py
+++ python/CreateHtmlNotes.py
@@ -654,6 +654,31 @@ class HtmlNotes:
 					htmlStr += "\n" + indentedNewLine(particle, htmlIndentation)
 					htmlIndentation -= 1
 					htmlStr += "\n" + indentedNewLine("</a>", htmlIndentation)
+					if(len(Particles[particle]["Concepts"]) > 1 
+						or len(Particles[particle]["Topics"]) > 0
+					):
+						htmlStr += "\n" + indentedNewLine("Also found in:", htmlIndentation)
+						for concept in Particles[particle]["Concepts"]:
+							if concept != Id:
+								ConceptFileName = "../../" + self.ConceptFolder + "/" + concept + ".html"
+								htmlStr += "\n" + indentedNewLine(
+									"<a href=\"{FileName:}\">".format(FileName = ConceptFileName), 
+									htmlIndentation
+								)
+								htmlIndentation += 1
+								htmlStr += "\n" + indentedNewLine(concept, htmlIndentation)
+								htmlIndentation -= 1
+								htmlStr += "\n" + indentedNewLine("</a>", htmlIndentation)
+						for topic in Particles[particle]["Topics"]:
+								TopicFileName = "../../" + self.TopicFolder + "/" + topic + ".html"
+								htmlStr += "\n" + indentedNewLine(
+									"<a href=\"{FileName:}\">".format(FileName = TopicFileName), 
+									htmlIndentation
+								)
+								htmlIndentation += 1
+								htmlStr += "\n" + indentedNewLine(topic, htmlIndentation)
+								htmlIndentation -= 1
+								htmlStr += "\n" + indentedNewLine("</a>", htmlIndentation)
 					htmlIndentation -= 1
 					htmlStr += "\n" + indentedNewLine("</h3>", htmlIndentation)
 					htmlStr += "\n" + indentedNewLine("<p>", htmlIndentation)
